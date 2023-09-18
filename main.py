66a2714c76ca7b1bcd1839942b99fcc75bb09268
Elif Lale <63200204+eliflali@users.noreply.github.com>
2023-09-18 14:19:52 +0300

    final version of main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ the top words in each news category.
 Author: Elif Lale
 """
 
+import argparse
 import logging
 from logging import getLogger, FileHandler, Formatter
 from json import loads
@@ -15,74 +16,69 @@ from html_generator import HtmlPage
 import string
 from threading import Thread
 
-# Set up logging to write to both console and log files
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-logger = getLogger()
-file_handler = FileHandler('logs/app.log')
-file_handler.setFormatter(Formatter('%(levelname)s: %(message)s'))
-logger.addHandler(file_handler)
+def configure_logging(log_file):
+  # Configure logging to write to both console and log files
+  logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+  logger = getLogger()
+  file_handler = FileHandler(log_file)
+  file_handler.setFormatter(Formatter('%(levelname)s: %(message)s'))
+  logger.addHandler(file_handler)
+  return logger
 
-# Function to process news data for a category
-def process_category(category, data_file, result_dict):
-  # Configure logging for this category
-  log_file = f'logs/{category}_log.txt'  # Using f-string for formatting
-  category_logger = getLogger(category)
-  category_logger.addHandler(FileHandler(log_file))
-  category_logger.setLevel(logging.INFO)
-
-  word_counts = {}
-
-  # Get NLTK stopwords
+def tokenize_headline(headline):
+  # Tokenize a headline based on word boundaries
+  #  and exclude stopwords and punctuation.
+  tokenizer = RegexpTokenizer(r'\b[a-zA-Z]+\b')
+  words = tokenizer.tokenize(headline)
   nltk_stopwords = set(stopwords.words('english'))
+  words = [word.lower() for word in words
+           if word.lower() not in nltk_stopwords
+           and word not in string.punctuation and len(word) > 1]
+  return words
+
+def process_category(category, data_file, result_dict):
+  # Process news data for a category and count top words.
+  log_file = f'logs/{category}_log.txt'
+  category_logger = configure_logging(log_file)
+  word_counts = {}
 
   with open(data_file, 'r', encoding='utf-8') as file:
     data = [loads(line) for line in file]
 
-    for item in data:
-      if item['category'] == category:
-        headline = item['headline']
+  for item in data:
+    if item['category'] == category:
+      words = tokenize_headline(item['headline'])
 
-        # Use NLTK to tokenize based on word boundaries
-        # Only match words containing alphabetic characters:
-        tokenizer = RegexpTokenizer(r'\b[a-zA-Z]+\b')
+      for word in words:
+        word = word.lower()
+        word_counts[word] = word_counts.get(word, 0) + 1
 
-        words = tokenizer.tokenize(headline)
-
-        # Exclude punctuation and one character length words
-        words = [word.lower() for word in words
-                if word.lower() not in nltk_stopwords
-                and
-                word not in string.punctuation and len(word) > 1]
-
-        for word in words:
-          word = word.lower()
-          if word not in nltk_stopwords:
-            word_counts[word] = word_counts.get(word, 0) + 1
-
-  # Log the top 10 words for the category
   top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:10]
   category_logger.info("Top words for category '%s': %s", category, top_words)
-
-  # Store the top words as a string in the result_dict
   result_dict[category] = '\n'.join([f'{word}: {count}' for word,
                                     count in top_words])
 
-# Main function
-def main():
-  #path to your dataset:
-  data_file_path = './data/News_Category_Dataset_v3.json'
+def extract_unique_categories(data_file):
+  # Extract unique news categories from the dataset.
   categories = set()
-
-  # Extract unique categories from the dataset
-  with open(data_file_path, 'r', encoding='utf-8') as file:
+  with open(data_file, 'r', encoding='utf-8') as file:
     data = [loads(line) for line in file]
 
-    for item in data:
-      categories.add(item['category'])
+  for item in data:
+    categories.add(item['category'])
 
-  # Create threads for each category
+  return categories
+
+def main():
+  # Parse command line arguments
+  parser = argparse.ArgumentParser(description='Process data.')
+  parser.add_argument('data_file', type=str, help='Path to the dataset file')
+  args = parser.parse_args()
+
+  data_file_path = args.data_file  # Use the provided dataset file path
+  categories = extract_unique_categories(data_file_path)
   threads = []
-  category_top_words = {}  # Store top words for each category
+  category_top_words = {}
 
   for category in categories:
     thread = Thread(target=process_category,
@@ -90,17 +86,15 @@ def main():
     threads.append(thread)
     thread.start()
 
-  # Wait for all threads to finish
   for thread in threads:
     thread.join()
 
-  # Generate HTML report
   html = HtmlPage('output.html')
   html.add_h1('Top Words in News Categories')
 
   for category, top_words_str in category_top_words.items():
     html.add_h1(category)
-    html.add_p(top_words_str)  # Add the top words as content to the paragraph
+    html.add_p(top_words_str)
 
   html.render()
 
