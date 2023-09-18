f2e418ed114dbad8476e351e6fec480e53b6abd7
Elif Lale <63200204+eliflali@users.noreply.github.com>
2023-09-18 14:07:10 +0300

    fixed issues using pylintrc

--- html_generator.py
+++ html_generator.py
@@ -1,22 +1,84 @@
+"""
+This module contains a class to
+arrange HTML operations.
+
+Author: Elif Lale
+"""
+
 from bs4 import BeautifulSoup
 
 class HtmlPage:
-    def __init__(self, filename):
-        self.filename = filename
-        self.html = BeautifulSoup('', 'html.parser')
-        self.body = self.html.new_tag('body')
-        self.html.append(self.body)
+  """
+  A utility class for creating and rendering HTML pages.
 
-    def add_h1(self, text):
-        h1 = self.html.new_tag('h1')
-        h1.string = text
-        self.body.append(h1)
+  This class allows you to easily create HTML pages 
+  and add content like headings (h1) and paragraphs (p).
+  After adding content, you can render the HTML page to a file.
 
-    def add_p(self, text):
-        p = self.html.new_tag('p')
-        p.string = text
-        self.body.append(p)
+  Args:
+      filename (str): The name of the HTML file to be generated.
 
-    def render(self):
-        with open(self.filename, 'w', encoding='utf-8') as file:
-            file.write(str(self.html))
+  Attributes:
+      filename (str): The name of the HTML file.
+      html (BeautifulSoup): The BeautifulSoup object 
+                            representing the HTML content.
+      body (Tag): The HTML body tag where content is added.
+
+  Example:
+      Create an HtmlPage object, add content, and render the HTML to a file.
+
+      >>> html = HtmlPage("output.html")
+      >>> html.add_h1("Welcome to My Website")
+      >>> html.add_p("This is a sample paragraph.")
+      >>> html.render()
+  """
+
+  def __init__(self, filename):
+    """
+    Initialize a new HtmlPage.
+
+    Args:
+        filename (str): The name of the HTML file to be generated.
+    """
+    self.filename = filename
+    self.html = BeautifulSoup('', 'html.parser')
+    self.body = self.html.new_tag('body')
+    self.html.append(self.body)
+
+  def add_h1(self, text):
+    """
+    Add an <h1> (heading 1) element to the HTML page.
+
+    Args:
+        text (str): The text content of the heading.
+
+    Example:
+        >>> html.add_h1("About Me")
+    """
+    h1 = self.html.new_tag('h1')
+    h1.string = text
+    self.body.append(h1)
+
+  def add_p(self, text):
+    """
+    Add a <p> (paragraph) element to the HTML page.
+
+    Args:
+        text (str): The text content of the paragraph.
+
+    Example:
+        >>> html.add_p("I enjoy programming and writing code.")
+    """
+    p = self.html.new_tag('p')
+    p.string = text
+    self.body.append(p)
+
+  def render(self):
+    """
+    Render the HTML content to the specified file.
+
+    Example:
+        >>> html.render()
+    """
+    with open(self.filename, 'w', encoding='utf-8') as file:
+      file.write(str(self.html))
--- main.py
+++ main.py
@@ -1,90 +1,108 @@
-import threading
+"""
+This module contains a script for processing 
+news data and generating an HTML report of 
+the top words in each news category.
+
+Author: Elif Lale
+"""
+
 import logging
-import json
-import nltk
-from bs4 import BeautifulSoup
+from logging import getLogger, FileHandler, Formatter
+from json import loads
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 from html_generator import HtmlPage
 import string
+from threading import Thread
 
 # Set up logging to write to both console and log files
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-logger = logging.getLogger()
-file_handler = logging.FileHandler('logs/app.log')
-file_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+logger = getLogger()
+file_handler = FileHandler('logs/app.log')
+file_handler.setFormatter(Formatter('%(levelname)s: %(message)s'))
 logger.addHandler(file_handler)
 
 # Function to process news data for a category
 def process_category(category, data_file, result_dict):
-    # Configure logging for this category
-    log_file = 'logs/{}_log.txt'.format(category)
-    category_logger = logging.getLogger(category)
-    category_logger.addHandler(logging.FileHandler(log_file))
-    category_logger.setLevel(logging.INFO)
+  # Configure logging for this category
+  log_file = f'logs/{category}_log.txt'  # Using f-string for formatting
+  category_logger = getLogger(category)
+  category_logger.addHandler(FileHandler(log_file))
+  category_logger.setLevel(logging.INFO)
 
-    word_counts = {}
+  word_counts = {}
 
-    # Get NLTK stopwords
-    nltk_stopwords = set(nltk.corpus.stopwords.words('english'))
+  # Get NLTK stopwords
+  nltk_stopwords = set(stopwords.words('english'))
 
-    with open(data_file, 'r', encoding='utf-8') as file:
-        data = [json.loads(line) for line in file]
+  with open(data_file, 'r', encoding='utf-8') as file:
+    data = [loads(line) for line in file]
 
-        for item in data:
-            if item['category'] == category:
-                headline = item['headline']
+    for item in data:
+      if item['category'] == category:
+        headline = item['headline']
 
-                # Use NLTK's regular expression tokenizer to tokenize based on word boundaries
-                tokenizer = nltk.RegexpTokenizer(r'\b[a-zA-Z]+\b')  # Only match words containing alphabetic characters
-                words = tokenizer.tokenize(headline)
+        # Use NLTK to tokenize based on word boundaries
+        # Only match words containing alphabetic characters:
+        tokenizer = RegexpTokenizer(r'\b[a-zA-Z]+\b')
 
-                # Filter out tokens that consist only of punctuation or are one character in length
-                words = [word.lower() for word in words if word.lower() not in nltk_stopwords and word not in string.punctuation and len(word) > 1]
+        words = tokenizer.tokenize(headline)
 
-                for word in words:
-                    word = word.lower()
-                    if word not in nltk_stopwords:
-                        word_counts[word] = word_counts.get(word, 0) + 1
+        # Exclude punctuation and one character length words
+        words = [word.lower() for word in words
+                if word.lower() not in nltk_stopwords
+                and
+                word not in string.punctuation and len(word) > 1]
 
-    # Log the top 10 words for the category
-    top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:10]
-    category_logger.info("Top words for category '{}': {}".format(category, top_words))
+        for word in words:
+          word = word.lower()
+          if word not in nltk_stopwords:
+            word_counts[word] = word_counts.get(word, 0) + 1
 
-    # Store the top words as a string in the result_dict
-    result_dict[category] = "\n".join([f"{word}: {count}" for word, count in top_words])
+  # Log the top 10 words for the category
+  top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+  category_logger.info("Top words for category '%s': %s", category, top_words)
+
+  # Store the top words as a string in the result_dict
+  result_dict[category] = '\n'.join([f'{word}: {count}' for word,
+                                    count in top_words])
 
 # Main function
 def main():
-    data_file_path = './data/News_Category_Dataset_v3.json'  # Path to your dataset
-    categories = set()
+  #path to your dataset:
+  data_file_path = './data/News_Category_Dataset_v3.json'
+  categories = set()
 
-    # Extract unique categories from the dataset
-    with open(data_file_path, 'r', encoding='utf-8') as file:
-        data = [json.loads(line) for line in file]
-        for item in data:
-            categories.add(item['category'])
+  # Extract unique categories from the dataset
+  with open(data_file_path, 'r', encoding='utf-8') as file:
+    data = [loads(line) for line in file]
 
-    # Create threads for each category
-    threads = []
-    category_top_words = {}  # Store top words for each category
+    for item in data:
+      categories.add(item['category'])
 
-    for category in categories:
-        thread = threading.Thread(target=process_category, args=(category, data_file_path, category_top_words))
-        threads.append(thread)
-        thread.start()
+  # Create threads for each category
+  threads = []
+  category_top_words = {}  # Store top words for each category
 
-    # Wait for all threads to finish
-    for thread in threads:
-        thread.join()
+  for category in categories:
+    thread = Thread(target=process_category,
+                    args=(category, data_file_path, category_top_words))
+    threads.append(thread)
+    thread.start()
 
-    # Generate HTML report
-    html = HtmlPage("output.html")
-    html.add_h1("Top Words in News Categories")
+  # Wait for all threads to finish
+  for thread in threads:
+    thread.join()
 
-    for category, top_words_str in category_top_words.items():
-        html.add_h1(category)
-        html.add_p(top_words_str)  # Add the top words as content to the paragraph
+  # Generate HTML report
+  html = HtmlPage('output.html')
+  html.add_h1('Top Words in News Categories')
 
-    html.render()
+  for category, top_words_str in category_top_words.items():
+    html.add_h1(category)
+    html.add_p(top_words_str)  # Add the top words as content to the paragraph
 
-if __name__ == "__main__":
-    main()
+  html.render()
+
+if __name__ == '__main__':
+  main()
